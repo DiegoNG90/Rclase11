83404961ffa49e43ba299c4e84ff03c8b7721e0b
Diego <diego.nicolas.gandara@gmail.com>
2021-08-22 20:48:59 -0300

    Me traigo el componente Loader del desafio y lo adapto con Typescript :)

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import BounceLoader from 'react-spinners/BounceLoader';
+import { Container } from "react-bootstrap";
+import './style.css';
+
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+const override = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '0 auto',
+  borderColor: 'green',
+};
+
+const Loading: React.FC = (): JSX.Element =>{
+  return(
+    <Container className="loader__size">
+      <BounceLoader color="green" css={override} size={250} />;
+    </Container>
+  )
+}
+
+export default Loading;
