bed2d8082a9a2a16ad9ada623615e962a170e933
Diego <diego.nicolas.gandara@gmail.com>
2021-08-22 21:08:59 -0300

    Renombro y muevo el componente: error de ubicacion xD

---
+++ src/Pages/Products/Products.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+
+import ProductList from "../../Components/ProductList";
+import Loader from '../../Components/Loader'
+import { useAxios } from "../../Hooks/useAxios";
+import { iProduct } from "../../interfaces/products";
+
+const Products: React.FC = (): JSX.Element => {
+  const [products, loading] : [any, boolean] = useAxios("search?q=zapatillas&limit=5");
+  const { results }: { results: Array<iProduct> } = products;
+
+  console.log(results);
+  if (loading) return <Loader />;
+
+  return (
+    <>
+      <Helmet>
+        <title>Productos</title>
+        <meta charSet="utf-8" />
+      </Helmet>
+
+      <Container>
+        <Row className="justify-content-center">
+          {results?.length > 0 ? (
+            results.map((product: git adiProduct) => (
+              <ProductList product={product} />
+            ))
+          ) : (
+            <h3>No hay productos</h3>
+          )}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Products;
